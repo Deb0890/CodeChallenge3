7722c3df80a8703dd97d3d26ebc16b9340d38c65
deb0890 <deborahf1@hotmail.com>
2021-08-11 14:57:47 +0100

    reducer folder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 
 
-const initState = {};
+const initState = { all: [{ name: "", }]};
 
 const repoReducer = (state=initState, action) => {
     switch(action.type) {
         case 'LOAD_REPOS':
-            return ({...state, repos         })
+            return ({...state, repos })
     }
 
 }
